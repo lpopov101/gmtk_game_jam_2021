dd4126a328f0fb08a6d02bad2c284f3d3707289a
William Johnston <william.benjamin.johnston@gmail.com>
2021-06-13 02:51:27 -0500

    Start menu screen added

--- GMTKGameJam2021/Assets/Source/UI/UIController.cs
+++ GMTKGameJam2021/Assets/Source/UI/UIController.cs
@@ -8,6 +8,7 @@ public class UIController : MonoBehaviour
     // Sub Panels
     [SerializeField] private GameObject _tutorialPanel;
     [SerializeField] private GameObject _overlayPanel;
+    [SerializeField] private GameObject _menuScreenPanel;
 
     // In Game Overlay
     [SerializeField] private Text _coinCountField;
@@ -16,11 +17,17 @@ public class UIController : MonoBehaviour
     [SerializeField] private Button _tutorialHideButton;
 
     // Start Menu
+    [SerializeField] private Button _menuScreenPlayButton;
+    [SerializeField] private Button _menuScreenTutorialButton;
 
     // Start is called before the first frame update
     void Start() {
         //_coinCountField.text = "Coins Collected: 32!";
-        _tutorialPanel.SetActive(true);
+        _tutorialPanel.SetActive(false);
+        _overlayPanel.SetActive(true);
+        _menuScreenPanel.SetActive(true);
+
+        setupMenuActions();
         setupTutorialActions();
     }
 
@@ -37,6 +44,31 @@ public class UIController : MonoBehaviour
         _tutorialPanel.SetActive(false);
     }
 
-
     // Start Menu Actions
+    private void setupMenuActions()
+    {
+        _menuScreenPlayButton.onClick.AddListener(() => {
+            startScreenPlayGame();
+        });
+
+        _menuScreenTutorialButton.onClick.AddListener(() => {
+            startScreenPresentTutorial();
+        });
+    }
+
+    private void startScreenPresentTutorial()
+    {
+        _menuScreenPanel.SetActive(false);
+        _tutorialPanel.SetActive(true);
+        _overlayPanel.SetActive(true);
+    }
+
+    private void startScreenPlayGame()
+    {
+        _menuScreenPanel.SetActive(false);
+        _overlayPanel.SetActive(true);
+        _tutorialPanel.SetActive(false);
+    }
+
+
 }
